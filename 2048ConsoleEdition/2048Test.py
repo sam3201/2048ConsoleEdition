1d40021fd4c29f4e1765f450f6bc499e6f83a88c
sam3201 <66579739+sam3201@users.noreply.github.com>
2024-02-09 22:47:21 -0500

    Update 2048Test.py

--- 2048ConsoleEdition/2048Test.py
+++ 2048ConsoleEdition/2048Test.py
@@ -1,37 +1,16 @@
-class Cell:
-    def __init__(self, val, grid_size):
-        self.val = val
-        self.grid_size = grid_size
+class Nothing:
+    def __init__(self):
+        self.__class__.__name__ = "Nothing"
+        self.toSee
+        self.here
 
-    def val_size(self, val):
-        if isinstance(val, int):
-            return len(str(val))
-        elif isinstance(val, str):
-            return len(val)
-        else:
-            print(f"Val: {val} is of type: {type(val)}")
-            return quit()
+    @classmethod
+    def toSee(cls, nothing, toSee, here):
+        print(nothing.__name__ + toSee.toSee.__name__ + here.here.__name__)
 
-    def print_cell(self):
-        str_size = self.val_size(self.val)
-        if str_size % 2 == 0:
-            middle = str_size // 2
-        else:
-            middle = (str_size // 2) + 1
+    @classmethod
+    def here(cls):
+        return str("h" + "e" + "r" + "e")
 
-        spaces = (self.grid_size - str_size) // 2
-        left_spaces = " " * spaces
-        right_spaces = " " * ((self.grid_size - str_size - spaces) + 1)
-
-        print("=" * (self.grid_size + 4))
-        for _ in range(spaces):
-            print("=" + " " * (self.grid_size + 2) + "=")
-        print("=" + " " + left_spaces + str(self.val) + right_spaces + "=")
-        for _ in range(spaces):
-            print("=" + " " * (self.grid_size + 2) + "=")
-        print("=" * (self.grid_size + 4))
-
-
-cell = Cell(val=1, grid_size=3)
-print(cell.val_size(cell.val))
-print(cell.print_cell(cell.val))
+if __name__ == "main":
+    __ = Nothing.toSee.here
