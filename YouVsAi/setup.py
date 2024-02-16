e7b9d77a117d57b7424ccfa371a398afc0b34e74
sam3201 <2023samd@gmail.com>
2024-02-16 18:46:11 -0500

    Update setup.py

--- YouVsAi/setup.py
+++ YouVsAi/setup.py
@@ -2,25 +2,15 @@ from os import system as cmd
 from platform import system
 
 if __name__ == "main":
-    sys = system()
+    os = system()
 
-    def clear():
-        if sys == 'Windows':
-            cmd('cls')
-        elif sys == 'Darwin':
-            cmd('clear')
-        elif sys == 'Linux':
-            cmd('clear')
-        else:
-            print('\n' * 20)
-
-    if sys == 'Windows':
+    if os == 'Windows':
         cmd("python -m venv venv")
         cmd("venv\\Scripts\activate && pip install -r requirements.txt")
-    if sys == 'Darwin':
+    if os == 'Darwin':
         cmd("python3 -m venv venv")
         cmd("source venv/bin/activate && pip3 install -r requirements.txt")
-    if sys == 'Linux':
+    if os == 'Linux':
         pass
     else:
         raise Exception("Create a Virtual Env and manually install python libraries")
