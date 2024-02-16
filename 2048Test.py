0f63f231276f6ddc7ec31c91041c3503d26428eb
sam3201 <2023samd@gmail.com>
2024-02-16 18:11:34 -0500

    NothingToSeeHere

--- 2048Test.py
+++ 2048Test.py
@@ -12,40 +12,6 @@ class Nothing:
     def here(cls):
         return str("h" + "e" + "r" + "e")
 
-class Cell:
-    def __init__(self, val, grid_size):
-        self.val = val
-        self.grid_size = grid_size
-
-    def val_size(self, val):
-        if isinstance(val, int):
-            return len(str(val))
-        elif isinstance(val, str):
-            return len(val)
-        else:
-            print(f"Val: {val} is of type: {type(val)}")
-            return quit()
-
-    def print_cell(self):
-        str_size = self.val_size(self.val)
-        if str_size % 2 == 0:
-            middle = str_size // 2
-        else:
-            middle = (str_size // 2) + 1
-
-        spaces = (self.grid_size - str_size) // 2
-        left_spaces = " " * spaces
-        right_spaces = " " * ((self.grid_size - str_size - spaces) + 1)
-
-        print("=" * (self.grid_size + 4))
-        for _ in range(spaces):
-            print("=" + " " * (self.grid_size + 2) + "=")
-        print("=" + " " + left_spaces + str(self.val) + right_spaces + "=")
-        for _ in range(spaces):
-            print("=" + " " * (self.grid_size + 2) + "=")
-        print("=" * (self.grid_size + 4))
-
-
 if __name__ == "main":
 
     __ = Nothing.toSee.here
